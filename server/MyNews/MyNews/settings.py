c92079a6ef0160ffbfa906d5d7b30b87e7a70b0a
Nelson-Fuentes <40514220+Nelson-Fuentes@users.noreply.github.com>
2020-06-04 14:45:19 -0500

    Contraseñas

--- server/MyNews/MyNews/settings.py
+++ server/MyNews/MyNews/settings.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
+correo: mnews1309@gmail.com
+contraseña  correo: myNews2020*
+
 """
 
 import os
