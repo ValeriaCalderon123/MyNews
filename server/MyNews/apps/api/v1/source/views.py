0f433a99ff83e208802732e6838a45b325da0e1b
Nelson-Fuentes <40514220+Nelson-Fuentes@users.noreply.github.com>
2020-07-22 21:19:45 -0500

    Correccion en calificacion de articulos

--- server/MyNews/apps/api/v1/source/views.py
+++ server/MyNews/apps/api/v1/source/views.py
@@ -3,6 +3,8 @@ from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from apps.article.models import Article
 from apps.source.serializers import SourceCreateUpdateSerializer, SourceShowSerializer
 from apps.source.models import Source
 from permissions.permissions import AdminAuthenticationPermission
@@ -23,22 +25,21 @@ class CalificateSourceAPIView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def put(self, request, pk):
-        if pk.isdigit():
-            source = get_object_or_404(Source, pk=pk)
-            calification = request.data.get('calification')
-            calification_num = 0
-            edit = False
-            if calification.__class__ == int and calification is not 0:
-                calification_num = calification / abs(calification)
-                edit = True
-            elif calification.__class__ == str and calification.isdigit():
-                edit = True
-                if int(calification) > 0:
-                    calification_num = 1
-                elif (calification) < 0:
-                    calification_num = -1
-            if edit:
-                source.calification = source.calification + calification_num
-                source.save()
-                return Response(SourceShowSerializer(source).data)
+        article = get_object_or_404(Article, pk=pk)
+        calification = request.data.get('calification')
+        calification_num = 0
+        edit = False
+        if calification.__class__ == int and calification is not 0:
+            calification_num = calification / abs(calification)
+            edit = True
+        elif calification.__class__ == str and calification.isdigit():
+            edit = True
+            if int(calification) > 0:
+                calification_num = 1
+            elif (calification) < 0:
+                calification_num = -1
+        if edit:
+            article.source.calification = article.source.calification + calification_num
+            article.source.save()
+            return Response(SourceShowSerializer(article.source).data)
         return Response({'detail': 'Bad Request'}, status=status.HTTP_400_BAD_REQUEST)
